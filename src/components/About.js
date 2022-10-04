d72183cbeaa9c6ddcef742a8ccb952aa8a8ebb3e
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-04 11:41:23 +0300

    Completed Assignment

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+function About( {image, about} ) {
+  return (
+    <aside>
+      {image ? (
+        <img src={image} alt="blog logo" />
+      ) : (
+        <img src="https://via.placeholder.com/215" alt="blog logo" />
+      )}
+      <p>{about}</p>
+    </aside>
+  );
+}
+
+export default About;
